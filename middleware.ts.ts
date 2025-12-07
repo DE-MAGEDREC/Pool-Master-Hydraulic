aa38e41a82dabaf9784d506b994ece13b0134a53
ENFRIN <de.consulting66@gmail.com>
2025-12-07 01:47:20 +0100

    Update

--- middleware.ts.ts
+++ middleware.ts.ts
@@ -6,21 +6,25 @@ export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient({ req, res });
 
+  // Récupérer la session
   const { data } = await supabase.auth.getSession();
   const session = data.session;
 
+  // Protéger seulement /calculator
   if (req.nextUrl.pathname.startsWith("/calculator") && !session) {
     const redirectUrl = req.nextUrl.clone();
     redirectUrl.pathname = "/auth/login";
     redirectUrl.searchParams.set("redirectedFrom", req.nextUrl.pathname);
+
     return NextResponse.redirect(redirectUrl);
   }
 
   return res;
 }
 
+// IMPORTANT : exclure tous les fichiers statiques, ET manifest.json
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|icon|icons|manifest.json|.*\\..*).*)",
+    "/((?!_next/static|_next/image|favicon.ico|icons|icon|manifest.json|.*\\..*).*)",
   ],
 };
